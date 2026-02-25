cea67ac021b0d1d4ac4f1edf511c81fe6975edc2
kupluksemua-web <kupluksemua@gmail.com>
2026-02-26 00:58:16 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,33 +1,17 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-app = Flask(__name__)
+db = SQLAlchemy()
 
-# Data stok sementara (simulasi database)
-inventory = [
-    {"nama": "Deterjen Cair", "stok": 10},
-    {"nama": "Parfum Laundry", "stok": 5},
-    {"nama": "Plastik Packing", "stok": 20}
-]
-
-@app.route('/')
-def index():
-    return render_template('index.html', items=inventory)
-
-@app.route('/tambah', methods=['POST'])
-def tambah_barang():
-    nama = request.form.get('nama')
-    stok = request.form.get('stok')
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./testdb.db'
     
-    if nama and stok:
-        inventory.append({"nama": nama, "stok": int(stok)})
-    
-    # Redirect kembali ke halaman utama setelah menambah
-    return redirect(url_for('index'))
+    db.init_app(app)
 
-@app.route('/cetak')
-def cetak():
-    # Mengirim data ke halaman khusus cetak
-    return render_template('struk_print.html', items=inventory)
+    from routes import register_routes
+    register_routes(app,db)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    migrate = Migrate(app, db)
+    return app
